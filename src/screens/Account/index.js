4d4de1a3538542b4cc6ca60f913d074d3fd80e7a
GayatriAkhilesh <gayatriponnu02@gmail.com>
2024-09-25 11:05:29 +0530

    Stylishnova

--- src/screens/Account/index.js
+++ src/screens/Account/index.js
@@ -1,15 +1,22 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './style';
-import {Image, Text, TouchableOpacity, View} from 'react-native';
+import {Image, Modal, Text, TouchableOpacity, View} from 'react-native';
 import CommonHeaderLeft from '../../components/CommonHeaderLeft';
 import {useNavigation} from '@react-navigation/native';
 import {useDimensionContext} from '../../context';
 import CustomTextInput from '../../components/CustomTextInput';
+import CustomButton from '../../components/CustomButton';
+import ImagePicker from 'react-native-image-crop-picker';
 
 const Account = () => {
   const navigation = useNavigation();
   const dimension = useDimensionContext();
   const responsiveStyle = style(dimension.windowWidth, dimension.windowHeight);
+
+  const [fName, setFName] = useState('');
+  const [modal, setModal] = useState(false);
+  const [modalChoose, setModalChoose] = useState(false);
+  const [profileImage, setProfileImage] = useState('');
 
   useEffect(() => {
     navigation.setOptions({
@@ -17,34 +24,146 @@ const Account = () => {
     });
   }, []);
 
+  const handleEditImage = () => {
+    setModalChoose(true);
+  };
 
+  const handlePickFromGallery = () => {
+    setModalChoose(false);
+    ImagePicker.openPicker({
+      width: 300,
+      height: 400,
+      cropping: true,
+    })
+      .then(image => {
+        console.warn(image);
+        setProfileImage(image.path ?? '');
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-  const handleEditImage = () => {};
+  const handlePickFromCamera = () => {
+    setModalChoose(false);
+    ImagePicker.openCamera({
+      width: 300,
+      height: 400,
+      cropping: true,
+    })
+      .then(image => {
+        console.log(image);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-  const handleOpenImage = () => {};
+  const handleOpenImage = () => {
+    setModal(!modal);
+  };
 
+  const handleUpdateProfile = () => {};
   return (
     <View style={responsiveStyle.container}>
       <Text style={responsiveStyle.head}>Gayatri Sivakumar</Text>
       <View style={responsiveStyle.userImage}>
         <TouchableOpacity onPress={handleOpenImage}>
-        <Image
-          source={require('../../assets/images/profile-drawer.jpeg')}
-          style={responsiveStyle.image}
-        />
-          </TouchableOpacity>
-          <TouchableOpacity style={responsiveStyle.editTouch} onPress={handleEditImage}>
-        <Image
-          source={require('../../assets/images/edit-image.png')}
-          style={responsiveStyle.edit}
-        />
+          <Image
+            source={
+              profileImage === ''
+                ? require('../../assets/images/profile-drawer.jpeg')
+                : {uri: profileImage}
+            }
+            style={responsiveStyle.image}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={responsiveStyle.editTouch}
+          onPress={handleEditImage}>
+          <Image
+            source={require('../../assets/images/edit-image.png')}
+            style={responsiveStyle.edit}
+          />
         </TouchableOpacity>
       </View>
 
-    <CustomTextInput 
-    handleText={text => setFname(text)}
-    placeholder="First Name"/>
+      <CustomTextInput
+        handleText={text => setFname(text)}
+        placeholder="First Name"
+      />
+      <CustomTextInput
+        handleText={text => setFname(text)}
+        placeholder="Last Name"
+      />
+      <CustomTextInput
+        type="email"
+        handleText={text => setFname(text)}
+        placeholder="Email Address"
+      />
+      <CustomTextInput
+        handleText={text => setFname(text)}
+        placeholder="Mobile Number"
+      />
+      <CustomButton
+        type="primary"
+        handleButtonPress={handleUpdateProfile}
+        buttonText={'Update Profile'}
+      />
 
+      <Modal visible={modal} onRequestClose={() => setModal(false)} transparent>
+        <View style={responsiveStyle.modalBg}>
+          <TouchableOpacity
+            onPress={() => setModal(false)}
+            style={responsiveStyle.close}>
+            <Image
+              source={require('../../assets/images/close.png')}
+              style={responsiveStyle.edit}
+            />
+          </TouchableOpacity>
+          <Image
+            style={responsiveStyle.bigImage}
+            source={
+              profileImage === ''
+                ? require('../../assets/images/profile-drawer.jpeg')
+                : {uri: profileImage}
+            }
+          />
+        </View>
+      </Modal>
+
+      <Modal
+        visible={modalChoose}
+        onRequestClose={() => setModalChoose(false)} transparent>
+        <View>
+          <View style={responsiveStyle.chooseView}>
+            <TouchableOpacity
+              onPress={() => setModalChoose(false)}
+              style={responsiveStyle.close}>
+              <Image
+                source={require('../../assets/images/close.png')}
+                style={responsiveStyle.edit}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handlePickFromCamera}
+              style={responsiveStyle.cameraHolder}>
+              <Image
+                source={require('../../assets/images/dslr-camera.png')}
+                style={responsiveStyle.camera}
+              />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={handlePickFromGallery}
+              style={responsiveStyle.galleryHolder}>
+              <Image
+                source={require('../../assets/images/gallery.png')}
+                style={responsiveStyle.gallery}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
--- src/screens/Account/style.js
+++ src/screens/Account/style.js
@@ -7,9 +7,9 @@ const style = (width, height) =>
       padding: 20,
     },
     head: {
-      fontFamily: 'Poppins-Black',
+      fontFamily: 'Poppins-SemiBold',
       fontSize: 25,
-      color: '#ee1b4d',
+      color: '#000',
       textAlign: 'center',
     },
     userImage: {
@@ -18,10 +18,20 @@ const style = (width, height) =>
       alignItems: 'center',
       marginVertical: 10,
     },
+    bigImage: {
+      width: width * 0.8,
+      height: width * 0.8,
+    },
     image: {
       width: width * 0.4,
       height: width * 0.4,
       borderRadius: width * 0.2,
+    },
+    modalBg: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'rgba(0,0,0,0.7)',
     },
     edit: {
       width: 30,
@@ -30,8 +40,50 @@ const style = (width, height) =>
     },
     editTouch: {
       position: 'absolute',
-      right:6,
+      right: 6,
       bottom: 2,
+    },
+    close: {
+      backgroundColor: '#fff',
+      borderRadius: 25,
+      position: 'absolute',
+      zIndex: 9,
+      right: 24,
+      top: height * 0.23,
+    },
+
+    chooseView: {
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      height: 100,
+      width: 220,
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+    },
+    cameraHolder: {
+      backgroundColor: '#ee1b4d',
+      height: 50,
+      width: 75,
+      borderRadius: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    galleryHolder: {
+      backgroundColor: '#ee1b4d',
+      height: 50,
+      width: 75,
+      borderRadius: 10,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    camera: {
+      height: 35,
+      width: 35,
+    },
+    gallery: {
+      height: 35,
+      width: 35,
     },
   });
 
